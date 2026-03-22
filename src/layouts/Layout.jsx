f50d471324cd116e8cbcb74775946d7908c8dcd5
Cheng <cucu199307@gmail.com>
2026-03-22 11:10:36 +0800

    feat(layout): improve navbar UX with sticky header and responsive toggle menu

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,12 +1,18 @@
 import { NavLink, Outlet, Link } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import minimalist_everyday_goods_logo from "../assets/images/minimalist_everyday_goods_logo.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { getAsyncCart, selectCartItemsCount } from "../slices/cartSlice";
 
 const Layout = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
   const { initialized } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
+
+  const closeNav = () => setIsNavExpanded(false);
+
+  const toggleNav = () => setIsNavExpanded(!isNavExpanded);
 
   useEffect(() => {
     if (!initialized) {
@@ -21,9 +27,9 @@ const Layout = () => {
   };
   return (
     <div className="d-flex flex-column min-vh-100">
-      <header className=" fixed-top border-bottom bg-white">
+      <header className=" sticky-top border-bottom bg-white">
         <div className="container">
-          <nav className="navbar navbar-expand">
+          <nav className="navbar navbar-expand-lg">
             {/* Logo */}
             <NavLink to="/" className="navbar-brand">
               <img
@@ -37,46 +43,66 @@ const Layout = () => {
               />
             </NavLink>
 
+            <button
+              className="navbar-toggler"
+              type="button"
+              onClick={toggleNav}
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
             {/* Menu */}
-            <div className="navbar-nav ms-auto">
-              <NavLink
-                className={({ isActive }) => getNavLinkClass(isActive)}
-                to="/"
-              >
-                首頁
-                <i className="bi bi-house-door"></i>
-              </NavLink>
-              <NavLink
-                className={({ isActive }) => getNavLinkClass(isActive)}
-                to="/products"
-              >
-                產品列表
-                <i className="bi bi-list-ul"></i>
-              </NavLink>
-              <NavLink
-                className={({ isActive }) => getNavLinkClass(isActive)}
-                to="/cart"
-              >
-                購物車
-                <span className="position-relative d-inline-block">
-                  <i className="bi bi-bag-check fs-4"></i>
-                  {cartItemsCount > 0 && (
-                    <span
-                      className="badge rounded-pill text-bg-danger position-absolute top-0 start-100 translate-middle"
-                      style={{ fontSize: "0.65rem", padding: "0.25em 0.45em" }}
-                    >
-                      {cartItemsCount > 99 ? "99+" : cartItemsCount}
-                    </span>
-                  )}
-                </span>
-              </NavLink>
-              <NavLink
-                className={({ isActive }) => getNavLinkClass(isActive)}
-                to="/login"
-              >
-                進入後台
-                <i className="bi bi-box-arrow-in-right"></i>
-              </NavLink>
+            <div
+              className={`collapse navbar-collapse ${isNavExpanded ? "show" : ""}`}
+              id="navbarNav"
+            >
+              <div className="navbar-nav ms-auto">
+                <NavLink
+                  className={({ isActive }) => getNavLinkClass(isActive)}
+                  to="/"
+                  onClick={closeNav}
+                >
+                  首頁
+                  <i className="bi bi-house-door"></i>
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) => getNavLinkClass(isActive)}
+                  to="/products"
+                  onClick={closeNav}
+                >
+                  產品列表
+                  <i className="bi bi-list-ul"></i>
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) => getNavLinkClass(isActive)}
+                  to="/cart"
+                  onClick={closeNav}
+                >
+                  購物車
+                  <span className="position-relative d-inline-block">
+                    <i className="bi bi-bag-check fs-4"></i>
+                    {cartItemsCount > 0 && (
+                      <span
+                        className="badge rounded-pill text-bg-danger position-absolute top-0 start-100 translate-middle"
+                        style={{
+                          fontSize: "0.65rem",
+                          padding: "0.25em 0.45em",
+                        }}
+                      >
+                        {cartItemsCount > 99 ? "99+" : cartItemsCount}
+                      </span>
+                    )}
+                  </span>
+                </NavLink>
+                <NavLink
+                  className={({ isActive }) => getNavLinkClass(isActive)}
+                  to="/login"
+                  onClick={closeNav}
+                >
+                  進入後台
+                  <i className="bi bi-box-arrow-in-right"></i>
+                </NavLink>
+              </div>
             </div>
           </nav>
         </div>
